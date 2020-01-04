23d10f86c097ed5092c5312d3e4250464bb62c60
Five <fivecc@qq.com>
2020-01-04 23:41:35 +0800

    20-1-4

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    accessKey : 'y124kkZQi1_pIwwGRPBKeR6Y_Gvyemg8VLuh5kmc',
+    secretKey : 'ZO51nvAAyClNpeZovpNkuRrM8tu96i2dbFUcSu0E',
+    bucket_lists: [
+        {'tsdcake': 'cdn.fivecc.cn'}
+    ]
+}
