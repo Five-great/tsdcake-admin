3752399e19ad95fe7dcfeb06ba9e8cc89f9041e0
Five <fivecc@qq.com>
2020-01-11 00:09:50 +0800

    20-1-10-13

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.post('/wxfiledata', function(req, res) {
     request.post({url: 'https://api.weixin.qq.com/merchant/common/upload_img?access_token=29_ZU32NGNgFf_cQYFiJlt7qA4l0iZ80igtj5LZ9RemlLuDkmOfbSPVzSy6M_Qo2MNe1TqX8FIJ1ZLA8YfIc_sAeGIiuPfGic8r6UBDS_eRZph7CZWnXDERjzESOhoGOSjAEASAE&filename=test.png',formData: req.file}, function (error, response, body) {  
     if (!error && response.statusCode == 200) {
       //res.send((typeof body==='object')?body : JSON.parse(body));
-      var cc=[req.file,body,req.data,res,response];
+      var cc=[req.file,body,req.data,response];
       res.send((typeof cc==='object')?cc: JSON.parse(cc));
     }
     res.send((typeof req.body==='object')?req.body: JSON.parse(req.body));
