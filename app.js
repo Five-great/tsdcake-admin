af83524917566b1c7e637c3c9f3f8e8a18d897be
Five <fivecc@qq.com>
2020-01-10 23:07:49 +0800

    20-1-10-13

--- app.js
+++ app.js
@@ -131,10 +131,11 @@ app.post('/wxpostdata', function(req, res) {
 app.post('/wxfiledata', function(req, res) {
   console.log('接收的数据data');
 
-    request.post({url: 'https://api.weixin.qq.com/merchant/common/upload_img?access_token=29_ZU32NGNgFf_cQYFiJlt7qA4l0iZ80igtj5LZ9RemlLuDkmOfbSPVzSy6M_Qo2MNe1TqX8FIJ1ZLA8YfIc_sAeGIiuPfGic8r6UBDS_eRZph7CZWnXDERjzESOhoGOSjAEASAE&filename=test.png',formData: req.body}, function (error, response, body) {  
+    request.post({url: 'https://api.weixin.qq.com/merchant/common/upload_img?access_token=29_ZU32NGNgFf_cQYFiJlt7qA4l0iZ80igtj5LZ9RemlLuDkmOfbSPVzSy6M_Qo2MNe1TqX8FIJ1ZLA8YfIc_sAeGIiuPfGic8r6UBDS_eRZph7CZWnXDERjzESOhoGOSjAEASAE&filename=test.png',formData: req.formData}, function (error, response, body) {  
     if (!error && response.statusCode == 200) {
       //res.send((typeof body==='object')?body : JSON.parse(body));
-      res.send((typeof req.body==='object')?req.body: JSON.parse(req.body));
+      var cc=[req.formData,body];
+      res.send((typeof cc==='object')?cc: JSON.parse(cc));
     }
     res.send((typeof req.body==='object')?req.body: JSON.parse(req.body));
    });
