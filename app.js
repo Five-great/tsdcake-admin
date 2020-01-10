7294e4dff02926a1bf6584c4d22cc059e389f309
Five <fivecc@qq.com>
2020-01-10 09:37:23 +0800

    20-1-10-2

--- app.js
+++ app.js
@@ -120,12 +120,12 @@ app.post('/upload', function(req, res) {
 app.post('/wxpostdata', function(req, res) {
   console.log('接收的数据ww');
   console.log(req.body);
-  axios.post(req.body.url,req.body.data).then(response => {
-    let json = CircularJSON.stringify(response.data);
-            res.send(json);
-  }).catch(err => {
-    console.log('axios occurs======================================================= ', err);
-  });
+  // axios.post(req.body.url,req.body.data).then(response => {
+  //   let json = CircularJSON.stringify(response.data);
+  //           res.send(json);
+  // }).catch(err => {
+  //   console.log('axios occurs======================================================= ', err);
+  // });
 });
 
 app.post('/wxgetdata', function(req, res) {
