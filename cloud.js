5825ccd89b6e9edc0ae0a867021634c650812b91
five <fivecc@qq.com>
2020-07-15 15:08:47 +0800

    xyh011

--- cloud.js
+++ cloud.js
@@ -10,6 +10,7 @@ AV.Cloud.afterSave('Comment', function (request) {
     
     // AT评论通知
     let pid =currentComment.get('pid');
+    let rid =currentComment.get('rid');
 
     if (!pid) {
         console.log("这条评论没有 @ 任何人");
@@ -24,9 +25,16 @@ AV.Cloud.afterSave('Comment', function (request) {
         } else {
             console.log(currentComment.get('nick') + " @ 了" + parentComment.get('nick') + ", 但被 @ 的人没留邮箱... 无法通知");
         }
+        
     }, function (error) {
         console.warn('好像 @ 了一个不存在的人!!!');
     });
+    query.get(rid).then(function (rootComment) {
+        rootComment.set('isNotified', true);
+        rootComment.save();
+    }, function (error) {
+        console.warn(error);
+    })
 });
 
 AV.Cloud.define('resend_mails', function(req) {
