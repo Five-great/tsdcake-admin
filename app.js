a0ca522280c3c386d43b4844ba64f3a2429f8a15
Five-great <43601963+Five-great@users.noreply.github.com>
2021-08-25 19:48:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.get('/logout', function(req, res) {
   
 //set app variable   
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", true);
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1')
